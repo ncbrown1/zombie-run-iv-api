60dba8ca110791175db491ad41bb09b928b2d49a
Nick Brown <nickbrown714@gmail.com>
2016-02-21 13:19:30 -0800

    heroku?

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from app import create_app
 
 if __name__ == "__main__":
     app = create_app(os.getenv('APP_CONFIG', 'default'))
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='zombie-run-iv.herokuapp.com', port=80)
