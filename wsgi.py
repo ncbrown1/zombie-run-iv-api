172c28150192408d7b48db89d82bf27245f6ed10
Nick Brown <nickbrown714@gmail.com>
2016-02-21 13:26:51 -0800

    heroku plz

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ from app import create_app
 if __name__ == "__main__":
     app = create_app(os.getenv('APP_CONFIG', 'default'))
     port = int(os.environ.get('PORT', 5000))
-    app.run(port=port)
+    app.run(host='0.0.0.0', port=port)
